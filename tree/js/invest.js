bc050e5537456c31f1dc48f3002ecfae6c34feae
Yida <442570303@qq.com>
2018-01-02 15:58:02 +0800

    style: 减少函数包裹

--- tree/js/invest.js
+++ tree/js/invest.js
@@ -1,60 +1,58 @@
-function renderTree() {
-    // 当前根节点 ID
-    const MID_NODE_ID = '24416401'; 
-    const API_L = './data/huawei_left.json'; 
-    const API_L_2 = './data/huawei_left2.json'; 
-    const API_R = './data/huawei_right.json'; 
-    const API_R_2 = './data/huawei_right2.json'; 
 
-    // 此处应该使用 async/await + Promise 但当前项目不允许
-    d3.json(API_L, (error, respL) => {
+// 当前根节点 ID
+const MID_NODE_ID = '24416401';
+const API_L = './data/huawei_left.json';
+const API_L_2 = './data/huawei_left2.json';
+const API_R = './data/huawei_right.json';
+const API_R_2 = './data/huawei_right2.json';
+
+// 此处应该使用 async/await + Promise 但当前项目不允许
+d3.json(API_L, (error, respL) => {
+    if (error) {
+        return console.error(error);
+    }
+
+    d3.json(API_R, (error, respR) => {
         if (error) {
             return console.error(error);
         }
 
-        d3.json(API_R, (error, respR) => {
-            if (error) {
-                return console.error(error);
-            }
+        if (typeof respL === 'string') {
+            respL = JSON.parse(respL);
+        }
+        if (typeof respR === 'string') {
+            respR = JSON.parse(respR);
+        }
 
-            if (typeof respL === 'string') {
-                respL = JSON.parse(respL);
-            }
-            if (typeof respR === 'string') {
-                respR = JSON.parse(respR);
-            }
+        var treeLeft = respL[MID_NODE_ID][0];
+        var treeRight = respR[MID_NODE_ID][0];
 
-            var treeLeft = respL[MID_NODE_ID][0];
-            var treeRight = respR[MID_NODE_ID][0];
+        try {
+            // 初始化
+            render(treeRight, treeLeft, MID_NODE_ID);
+        } catch (error) {
+            console.error(error);
+        }
 
-            try {
-                // 初始化
-            	render(treeRight, treeLeft, MID_NODE_ID);
-            } catch (error) {
-                console.error(error);
-            }
-            
-        });
     });
-}
+});
 
-renderTree();
 
 // 渲染簇图
 function render(treeRight = {}, treeLeft = {}, MID_NODE_ID) {
-    
+
     const INIT_SCALE = .7;
-	const width = 1162;
+    const width = 1162;
     const height = window.innerHeight;
-    
-    
+
+
     const maxLength = Math.max(treeRight.children.length, treeLeft.children.length);
 
     var m = [20, 120, 20, 120];
     var w = 1280 - m[1] - m[3];
     var h = 600 - m[0] - m[2];
     var i = 0;
-    
+
     const gap = 0.05 * maxLength; // 树枝密度
 
     var tree = d3.layout.cluster()
@@ -73,30 +71,30 @@ function render(treeRight = {}, treeLeft = {}, MID_NODE_ID) {
     const zoom = d3.behavior.zoom()
         .scaleExtent([0.25, 2])
         .on('zoom', zoomFn);
-    
+
     // SVG 画布
     var svg = d3.select('#tree').append('svg:svg')
         .attr('width', width)
         // .attr('height', h + m[0] + m[2])
         .attr('height', Math.max((h + m[0] + m[2]) * gap, height))
-	    .call(zoom)
-	    .on('dblclick.zoom', null);
-    
-     var container = svg.append('svg:g')
-        .attr('transform', 'translate(' + h + ',' + (m[0]) + ')scale('+ INIT_SCALE + ')')
+        .call(zoom)
+        .on('dblclick.zoom', null);
+
+    var container = svg.append('svg:g')
+        .attr('transform', 'translate(' + h + ',' + (m[0]) + ')scale(' + INIT_SCALE + ')')
         .attr('class', 'container');
 
-	function zoomFn() {
-	    const {
+    function zoomFn() {
+        const {
 	        translate,
-	        scale
+            scale
 	    } = d3.event;
-	    const [x, y] = translate;
-	    const [dx, dy] = [h, m[0]];
-	    const zoomTranslate = [x + dx, y + dy];
-	    container.attr('transform', 'translate(' + zoomTranslate + ')scale(' + scale * INIT_SCALE + ')');
-	}
-	
+        const [x, y] = translate;
+        const [dx, dy] = [h, m[0]];
+        const zoomTranslate = [x + dx, y + dy];
+        container.attr('transform', 'translate(' + zoomTranslate + ')scale(' + scale * INIT_SCALE + ')');
+    }
+
     // 标识左侧树枝
     treeLeft = defLeftNode(treeLeft);
 
@@ -245,9 +243,9 @@ function render(treeRight = {}, treeLeft = {}, MID_NODE_ID) {
 
 }
 
- // 标识左侧树枝
- function defLeftNode(node) {
-    return deepAssign(node, {direction: -1});
+// 标识左侧树枝
+function defLeftNode(node) {
+    return deepAssign(node, { direction: -1 });
 }
 
 // 深拷贝并扩展属性
@@ -260,9 +258,9 @@ function deepAssign(obj, sub = {}) {
     if (isIterable(obj)) {
         newObj = isArray(obj) ? [] : {};
         for (let [k, v] of Object.entries(obj)) {
-            if (isIterable(v)) { 
+            if (isIterable(v)) {
                 newObj[k] = deepAssign(v, sub); // 递归
-                if(isObject(v)) {
+                if (isObject(v)) {
                     Object.assign(newObj[k], sub);
                 }
             } else {
@@ -272,9 +270,9 @@ function deepAssign(obj, sub = {}) {
     } else {
         return obj;
     }
-    if(isObject(newObj)) {
+    if (isObject(newObj)) {
         Object.assign(newObj, sub);
     }
-    
+
     return newObj;
 }

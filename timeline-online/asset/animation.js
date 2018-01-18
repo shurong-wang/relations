af15601d406898eae7fb9de828bad767f5ec0852
Yida <442570303@qq.com>
2018-01-18 19:42:28 +0800

    feat: 提取项目中的时间轴代码

---
+++ timeline-online/asset/animation.js
@@ -0,0 +1,22 @@
+(function (window) {
+	window.animation = function(fps){
+		if(fps) this.fps = fps;
+	}
+	animation.prototype.animationList = [];
+	animation.prototype.fps = 60;
+	animation.prototype.stop = function (index) {
+		clearInterval(index);
+		this.animationList = this.animationList.map(function (d) {
+			return d != index;
+		});
+	}
+	animation.prototype.start = function (render, fps) {
+		return this.animationList.push(setInterval(render, 1000/fps || this.fps));
+	}
+	animation.prototype.stopAll = function () {
+		this.animationList.forEach(function (d) {
+			return clearInterval(d);
+		})
+		this.animationList.splice(0);
+	}
+})(window)
